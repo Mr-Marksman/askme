330f808800daeb51a61fd17d886ae7b4b3a4ee06
Mr-Marksman <levshin.maximka@gmail.com>
2022-08-26 17:15:16 +0300

    Split question text

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def display_nickname(user)
     "@#{user.nickname}"
   end
+
+  def splited_question_body(question_body)
+    question_body.gsub("\n", "<br>").html_safe
+  end
 end
