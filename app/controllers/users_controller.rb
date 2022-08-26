750ed4ddf01a012529c2a0f4609730413e9fe4b7
Mr-Marksman <levshin.maximka@gmail.com>
2022-08-26 13:52:37 +0300

    Registration validates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,51 @@ class UsersController < ApplicationController
   end
 
   def create
-    user_params = params.require(:user).permit(:name, :nickname, :email, :password)
+    @user = User.new(user_params)
 
-    user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
 
-    session[:user_id] = user.id
+      redirect_to questions_url, notice: "User with nickname #{user_params[:nickname]} successfully created!"
+    else
+      flash.now[:alert] = "You filled in the registration fields incorrectly"
 
-    redirect_to questions_url, notice: "User with nickname #{user_params[:nickname]} successfully created!"
+      render :new
+    end
+  end
+
+  def edit 
+    @user=User.find(params[:id])
+  end
+
+  def update
+    @user=User.find(params[:id])
+
+    if @user.update(user_params)
+      session[:user_id] = @user.id
+
+      redirect_to questions_url, notice: "User data has updated"
+    else
+      flash.now[:alert] = "Errors occurred while trying to update the user"
+
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+
+    nickname = @user.nickname
+
+    @user.destroy
+    session.delete(:user_id)
+
+    redirect_to questions_path, notice: "User with nickname #{nickname} has deleted"
+  end
+
+  private
+
+  def user_params
+    user_params = params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,28 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, 
+    presence: true, 
+    uniqueness: true, 
+    format: { 
+      with: /\A^[a-z\d_+.\-]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+$\z/i, 
+      message: ": Please, put Email in right format: example.email@example.com" 
+    }
+
+  validates :nickname, 
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 40, message: ": length must be less than 40 symbols" },
+    format: { 
+      with: /\A[[:alnum:]_]+\z/i, 
+      message: ': Put only latin letters, numbers and "_"' 
+    }
+
+  validates :name, presence: true
+
+  before_save :downcase_nickname
+
+  def downcase_nickname
+    nickname.downcase!
+  end
 end
