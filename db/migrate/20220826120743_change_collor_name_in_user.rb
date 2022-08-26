1d79d5aa4d26830112e9d4e0d4a2e92d0029d34f
Mr-Marksman <levshin.maximka@gmail.com>
2022-08-26 16:02:35 +0300

    header color choise

--- db/migrate/20220826120743_change_collor_name_in_user.rb
+++ db/migrate/20220826120743_change_collor_name_in_user.rb
@@ -1,5 +1,5 @@
 class ChangeCollorNameInUser < ActiveRecord::Migration[7.0]
   def change
-    rename_column :Users, :header_collor, :color
+    rename_column :users, :header_collor, :color
   end
 end
